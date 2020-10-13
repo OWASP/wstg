24c0e8a2fe8efad98370e4f2d11cd7f71b532b27
Rejah Rehim <rejah@appfabs.com>
2020-10-13 10:07:25 +0530

    Update review comments

--- json/scripts/make-json.sh
+++ json/scripts/make-json.sh
@@ -4,9 +4,9 @@
 cd document/4-Web_Application_Security_Testing
 
 # Start the json string
-echo "{" > testing-checklist.json
+echo "{" > checklist.json
 # Open categories
-echo "    \"categories\": {"  >> testing-checklist.json
+echo "    \"categories\": {"  >> checklist.json
 # Iterate all category folders to get the categories
 for d in */ ; do
     # Split the folder name to get category and category number
@@ -18,24 +18,24 @@ for d in */ ; do
             # Start category sub section
             # Add coma from the second entry of the list
             if [ ${FOLD[0]} -gt 1 ];then
-                echo "        ,\"${category%?}\": {" | tr '_' ' '  >> testing-checklist.json
+                echo "        ,\"${category%?}\": {" | tr '_' ' '  >> checklist.json
             else
-                echo "        \"${category%?}\": {" | tr '_' ' '  >> testing-checklist.json
+                echo "        \"${category%?}\": {" | tr '_' ' '  >> checklist.json
             fi
             # Get category ID from the first file
             cid=`cat $d/01-* | grep "|WSTG-.*" | cut -d "|" -f 2 | sed 's/-01//'`
             # Add category ID and start tests
-            echo "            \"id\":\"${cid}\","  >> testing-checklist.json
-            echo "            \"tests\":["  >> testing-checklist.json
+            echo "            \"id\":\"${cid}\","  >> checklist.json
+            echo "            \"tests\":["  >> checklist.json
             count=0
             for file in $d*.md; do
                 # Remove README
                 if [[ $file != *"README.md" ]];then
                     # Add coma from the second entry of the list
                     if [ $count -gt 0 ];then
-                        echo "                ,{"  >> testing-checklist.json
+                        echo "                ,{"  >> checklist.json
                     else
-                        echo "                {"  >> testing-checklist.json
+                        echo "                {"  >> checklist.json
                     fi
                     # Get test ID, test name and reference link from the file
                     tid=`cat $file | grep "|WSTG-.*" | cut -d "|" -f 2`
@@ -43,24 +43,24 @@ for d in */ ; do
                     tname=${tname:2}
                     tref=`echo $file | sed 's/.md/.html/'`
                     # Add test ID, test name and reference link from the file
-                    echo "                \"name\":\"${tname}\","  >> testing-checklist.json
-                    echo "                \"id\":\"${tid}\","  >> testing-checklist.json
-                    echo "                \"Reference\":\"https://owasp.org/www-project-web-security-testing-guide/stable/4-Web_Application_Security_Testing/$tref\""  >> testing-checklist.json
-                    echo "                }"  >> testing-checklist.json
+                    echo "                \"name\":\"${tname}\","  >> checklist.json
+                    echo "                \"id\":\"${tid}\","  >> checklist.json
+                    echo "                \"Reference\":\"https://owasp.org/www-project-web-security-testing-guide/stable/4-Web_Application_Security_Testing/$tref\""  >> checklist.json
+                    echo "                }"  >> checklist.json
                     count=$((count+1))
                 fi
             done
             # Close tests list
-            echo "            ]"  >> testing-checklist.json
+            echo "            ]"  >> checklist.json
             # Close category sub section
-            echo "        }"  >> testing-checklist.json
+            echo "        }"  >> checklist.json
         fi
     done <<< "$d"
 done
 # Close categories
-echo "    }"  >> testing-checklist.json
+echo "    }"  >> checklist.json
 # End Json string
-echo "}"  >> testing-checklist.json
+echo "}"  >> checklist.json
 
 # Move generated file to checklist folder
-mv testing-checklist.json ../../checklist/Testing-checklist.json
+mv checklist.json ../../checklist/.
